7ff4eb78653113d08ba286ccb235523d3f352baa
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2019-04-06 13:44:09 +0900

    入力欄の透明化

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/sclevine/agouti"
+	"golang.org/x/crypto/ssh/terminal"
 	"log"
+	"syscall"
 	"time"
 )
 
@@ -11,13 +13,11 @@ func main() {
 	url := "https://www.amazon.co.jp/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=jpflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.co.jp%2Fgp%2Fhelp%2Fcustomer%2Fdisplay.html%2Fref%3Dnav_signin%3FnodeId%3D201945460&switch_account="
 	driver := agouti.ChromeDriver(agouti.Browser("chrome"))
 
-	var email, password string
+	fmt.Println("emailを入力してください: ")
+	email, _ := terminal.ReadPassword(int(syscall.Stdin))
 
-	fmt.Print("emailを入力してください: ")
-	fmt.Scan(&email)
-
-	fmt.Print("passwordを入力してください: ")
-	fmt.Scan(&password)
+	fmt.Println("passwordを入力してください: ")
+	password, _ := terminal.ReadPassword(int(syscall.Stdin))
 
 	err := driver.Start()
 	if err != nil {
@@ -43,9 +43,7 @@ func main() {
 	ap_email := page.FindByID("ap_email")
 	ap_password := page.FindByID("ap_password")
 
-	//password, _ := terminal.ReadPassword(int(syscall.Stdin))
-
-	ap_email.Fill(email)
+	ap_email.Fill(string(email))
 	ap_password.Fill(string(password))
 	time.Sleep(1 * time.Second)
 
